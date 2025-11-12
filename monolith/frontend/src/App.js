424a751627e7ce84a5c828338d693727278bb4e2
Dinu Ionica <dinuionirinel10@gmail.com>
2025-11-12 20:04:10 +0200

    Integrated the frontend configuration

---
+++ monolith/frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='text-red-400 text-5xl mt-32'> Initial commit</div>
+    </div>
+  );
+}
+
+export default App;
